7fd374ff6fb18f39c0fff6b27befb52efc1ba1bd
epic ebu val soft <114816071+Birdddddragon@users.noreply.github.com>
2023-09-23 12:56:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import pyautogui as pag
 import keyboard
+from tkinter import *
+
+
+tankselected = 0
+
+root = Tk()
+root.title('autotank 0')
+root.geometry('500x600')
+root.resizable(width=False, height=False)
+root.iconbitmap('autotank.ico')
+
+tank1image = PhotoImage(file="tank1.png")
+tank2image = PhotoImage(file="tank2.png")
+
+label1 = Label(root, text=("танг выбраный:",tankselected), font=40)
+label1.pack(side=BOTTOM)
+
+label2 = Label(root, text=("чтобы начать процесс самоуничтожения системы нажмите alt + 1"), font=1)
+label2.pack(side=BOTTOM)
+
 
 pag.FAILSAFE = False
 timepause = 0.20
@@ -20,81 +40,123 @@ enginetab = (1350,80)
 gearboxtab = (1450,80)
 devicestab = (744,1040)
 
+print ("автотанк запущен ")
+
+def tank1buttonclick():
+    global tankselected
+    print("я даун")
+    tankselected=1
+    label1.config(text= ("танг выбраный:",tankselected))
+
+def tank2buttonclick():
+    global tankselected
+    print("я даун 2")
+    tankselected=2
+    label1.config(text= ("танг выбраный:",tankselected))
+
 def tankbuild():
-    # каркас ебучий
-    pag.dragRel(0,-200, timepause, button='left')
-    pag.PAUSE = timepause2
-    pag.moveRel(0,90, timepause)
-    pag.PAUSE = timepause2
-    pag.dragRel(0,-200, timepause, button='left')
-    pag.PAUSE = timepause2
-    pag.moveRel(90,150, timepause)
-    pag.dragRel(140,0, timepause3)
-    pag.PAUSE = timepause2
-    pag.moveRel(-310,0, timepause)
-    pag.dragRel(-145,0, timepause3)
-    pag.PAUSE = timepause2
-    # колеса даунские
-    pag.moveRel(110,40, timepause2)
-    wheel1=(pag.position())
-    pag.PAUSE = timepause2
-    pag.moveRel(240,0, timepause2)
-    wheel2=(pag.position())
-    print (wheel1,wheel2)
-    pag.moveTo(devicestab)
-    pag.mouseDown()
-    pag.mouseUp()
-    pag.moveTo(smallwheel)
-    # колеса даунские
-    pag.mouseDown()
-    pag.mouseUp()
-    pag.moveTo(wheel1)
-    pag.mouseDown()
-    pag.mouseUp()
-    pag.moveTo(wheel2)
-    pag.mouseDown()
-    pag.mouseUp()
-    # контролер блядский
-    pag.moveRel(-120,-15)
-    control=(pag.position())
-    pag.PAUSE = timepause2
-    pag.moveTo(devicestab)
-    pag.mouseDown()
-    pag.mouseUp()
-    keyboard.press('q')
-    pag.moveTo(vehiclecontrol)
-    pag.mouseDown()
-    pag.mouseUp()
-    keyboard.release('q')
-    pag.moveTo(control)
-    pag.mouseDown()
-    pag.mouseUp()
-    # двигатель ебаный
-    pag.moveRel(-130,-150)
-    engine=(pag.position())
-    keyboard.press('q')
-    pag.moveTo(enginetab)
-    pag.mouseDown()
-    pag.mouseUp()
-    keyboard.release('q')
-    pag.moveTo(engine)
-    pag.mouseDown()
-    pag.mouseUp()
-    # каробка ебаная
-    pag.moveRel(150,0)
-    gearbox=(pag.position())
-    keyboard.press('q')
-    pag.moveTo(gearboxtab)
-    pag.mouseDown()
-    pag.mouseUp()
-    keyboard.release('q')
-    pag.moveTo(gearbox)
-    pag.mouseDown()
-    pag.mouseUp()
+    if tankselected == 1:
+        # каркас
+        pag.dragRel(0,-200, timepause, button='left')
+        pag.PAUSE = timepause2
+        pag.moveRel(0,90, timepause)
+        pag.PAUSE = timepause2
+        pag.dragRel(0,-200, timepause, button='left')
+        pag.PAUSE = timepause2
+        pag.moveRel(90,150, timepause)
+        pag.dragRel(140,0, timepause3)
+        pag.PAUSE = timepause2
+        pag.moveRel(-310,0, timepause)
+        pag.dragRel(-145,0, timepause3)
+        pag.PAUSE = timepause2
+        # колеса
+        pag.moveRel(110,40, timepause2)
+        wheel1=(pag.position()) 
+        pag.PAUSE = timepause2
+        pag.moveRel(240,0, timepause2)
+        wheel2=(pag.position())
+        print (wheel1,wheel2)
+        pag.moveTo(devicestab)
+        pag.mouseDown()
+        pag.mouseUp()
+        pag.moveTo(smallwheel)
+        # колеса
+        pag.mouseDown()
+        pag.mouseUp()
+        pag.moveTo(wheel1)
+        pag.mouseDown()
+        pag.mouseUp()
+        pag.moveTo(wheel2)
+        pag.mouseDown()
+        pag.mouseUp()
+        # контролер
+        pag.moveRel(-120,-15)
+        control=(pag.position())
+        pag.PAUSE = timepause2
+        pag.moveTo(devicestab)
+        pag.mouseDown()
+        pag.mouseUp()
+        keyboard.press('q')
+        pag.moveTo(vehiclecontrol)
+        pag.mouseDown()
+        pag.mouseUp()
+        keyboard.release('q')
+        pag.moveTo(control)
+        pag.mouseDown()
+        pag.mouseUp()
+        # двигатель
+        pag.moveRel(-130,-110)
+        engine=(pag.position())
+        keyboard.press('q')
+        pag.moveTo(enginetab)
+        pag.mouseDown()
+        pag.mouseUp()
+        keyboard.release('q')
+        pag.moveTo(engine)
+        pag.mouseDown()
+        pag.mouseUp()
+        # каробка
+        pag.moveRel(150,0)
+        gearbox=(pag.position())
+        keyboard.press('q')
+        pag.moveTo(gearboxtab)
+        pag.mouseDown()
+        pag.mouseUp()
+        keyboard.release('q')
+        pag.moveTo(gearbox)
+        pag.mouseDown()
+        pag.mouseUp()
+    if tankselected == 2:
+        pag.dragRel(0,-200, timepause, button='left')
+        pag.PAUSE = timepause2
+        pag.moveRel(0,90, timepause)
+        pag.PAUSE = timepause2
+        pag.dragRel(0,-200, timepause, button='left')
+        pag.PAUSE = timepause2
+        pag.moveRel(90,150, timepause)
+        pag.dragRel(140,0, timepause3)
+        pag.PAUSE = timepause2
+        pag.moveRel(-310,0, timepause)
+        pag.dragRel(-145,0, timepause3)
+        pag.PAUSE = timepause2
+
+
+
+tank1button = Button(root, text="танг адин", font=50, command=tank1buttonclick, image=tank1image)
+tank1button.pack()
+
+tank2button = Button(root, text="танг дыва", font=50, command=tank2buttonclick, image=tank2image)
+tank2button.pack()
 
 keyboard.add_hotkey('Alt + 1', tankbuild)
 
 
-keyboard.wait('Ctrl + Q')
+root.mainloop()
+
+
+
+
+
+#keyboard.wait('Ctrl + Q')
 
 
